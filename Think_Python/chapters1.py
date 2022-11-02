0aadd551c6bc28fe13fb5c741fa4f17dd785659d
Nik <Nik@DESKTOP-VBHJGBJ>
2022-11-02 23:02:16 +0200

    Редакция

--- Think_Python/chapters1.py
+++ Think_Python/chapters1.py
@@ -1,11 +1,14 @@
 
 Second = (42 * 60 + 42)
 
-Minute = (Second / 60)
+Mil = (10 * 1.62)
 
-mil = (10 * 1.62)
+print("Секунд в 42минутах и 42 Секунд:" Second)
 
+print("Сколько Миль в 10 км:" Mil)
+Second = (42.42 / 10) 
+Runing = (Second * 1.6)
+Speed = (60 / mile)
 
-
-print(Minute / 10)
-
+print("Средний темп бега:" Runing)
+print("Средня Скорость бега" Speed)

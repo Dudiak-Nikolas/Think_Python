853f64b18ad16f549fd9cb3b2cfff7bc56963af2
Nik <Nik@DESKTOP-VBHJGBJ>
2022-11-04 00:21:27 +0200

    Redact

---
+++ Think_Python/Chapters2.py
@@ -0,0 +1,19 @@
+import math
+radius = 5
+pi = 3.141592653589793
+v = (4/3) * pi * math.pow(radius , 3)
+print(v)
+
+
+books = (249.5 - (249.5 / 100 * 40)) * 60
+delivery = 49.5 * 59
+purchase = books + delivery + 100
+print (purchase)
+
+
+area1 = (8 * 60) + 15
+area2 = (7 * 60) + 12
+total = ((area1 * 2) + (area2 * 3)) / 60
+time = ((6 * 60) + 52 + total) / 60
+Minuts = ((time - 7) * 60) / 100 * 1 + 7
+print (Minuts)
--- Think_Python/chapters1.py
+++ Think_Python/chapters1.py
@@ -1,14 +1,13 @@
 
-Second = (42 * 60 + 42)
+Second_for_min = (42 * 60 + 42)
 
 Mil = (10 * 1.62)
 
-print("Секунд в 42минутах и 42 Секунд:" Second)
-
-print("Сколько Миль в 10 км:" Mil)
 Second = (42.42 / 10) 
 Runing = (Second * 1.6)
-Speed = (60 / mile)
+Speed = (60 / Runing)
 
-print("Средний темп бега:" Runing)
-print("Средня Скорость бега" Speed)
+print (Second);
+print (Mil);
+print (Runing);
+print (Speed);
